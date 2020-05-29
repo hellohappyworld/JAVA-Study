dfb7c8d1cbe9b0a22983fc12c4d1f7beca3ff113
gaowj <gaowj@ifeng.com>
2020-05-29 10:17:40 +0800

    单节点redis数据同步

--- src/main/java/com/gaowj/SingleRedisDataMove.java
+++ src/main/java/com/gaowj/SingleRedisDataMove.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -20,64 +21,26 @@ import java.util.concurrent.Executors;
  */
 public class SingleRedisDataMove {
     public static void main(String[] args) {
-        String filePath = args[0];
-        List<String> list = new ArrayList<String>();
-        try {
-            String encoding = "GBK";
-            File file = new File(filePath);
-            if (file.isFile() && file.exists()) { // 判断文件是否存在
-                InputStreamReader read = new InputStreamReader(
-                        new FileInputStream(file), encoding);// 考虑到编码格式
-                BufferedReader bufferedReader = new BufferedReader(read);
-                String lineTxt = null;
+        String fromHost = args[0];
+        int formPost = Integer.parseInt(args[1]);
+        String fromMessage = args[2];
+        String fromDb = args[3];
 
-                while ((lineTxt = bufferedReader.readLine()) != null) {
-                    list.add(lineTxt);
-                }
-                bufferedReader.close();
-                read.close();
-            } else {
-                System.out.println("找不到指定的文件");
-            }
+        Jedis fromJedis = new Jedis(fromHost, formPost);
+        fromJedis.auth(fromMessage);
+        fromJedis.select(Integer.parseInt(fromDb));
 
-            ExecutorService es = Executors.newFixedThreadPool(60);
-            for (String line : list) {
-                String[] split = line.split(",");
-                String fromJedisOption = split[0];
-                String fromDb = split[1];
-                String toJedisOption = split[2];
-                String toDb = split[3];
-                Jedis fromJedis = null;
-                Jedis toJedis = null;
-                switch (fromJedisOption) {
-                    case "11":
-                        fromJedis = RedisPool.getJedisPool11();
-                        fromJedis.select(Integer.parseInt(fromDb));
-                        break;
-                    case "12":
-                        fromJedis = RedisPool.getJedisPool12();
-                        fromJedis.select(Integer.parseInt(fromDb));
-                        break;
-                    case "13":
-                        fromJedis = RedisPool.getJedisPool13();
-                        fromJedis.select(Integer.parseInt(fromDb));
-                        break;
-                }
-                switch (toJedisOption) {
-                    case "125":
-                        toJedis = RedisPool.getJedis125_7001(Integer.parseInt(toDb));
-                        break;
-                    case "126":
-                        toJedis = RedisPool.getJedis126_7001(Integer.parseInt(toDb));
-                        break;
-                }
+        String toHost = args[4];
+        int toPost = Integer.parseInt(args[5]);
+        String toMessage = args[6];
+        String toDb = args[7];
 
-                es.submit(new SingleRedisDataMoveJob(fromJedis, fromDb, toJedis, toDb));
-            }
+        Jedis toJedis = new Jedis(toHost, toPost);
+        toJedis.auth(toMessage);
+        toJedis.select(Integer.parseInt(toDb));
 
-        } catch (Exception e) {
-            System.out.println("读取文件内容出错");
-            e.printStackTrace();
-        }
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        es.submit(new SingleRedisDataMoveJob(fromJedis, fromDb, toJedis, toDb));
+
     }
 }
